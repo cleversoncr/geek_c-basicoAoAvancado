46113a1120ff0d08905cb795108d52e6a6a96a89
cleversoncr <cleversoncr@gmail.com>
2022-11-19 16:43:00 -0300

    Estruturas de decisão em C

---
+++ programCFont/decisao_ifElse.c
@@ -0,0 +1,19 @@
+//Estruturas de decisão if, else, else if
+#include <stdio.h>
+
+int main(){
+	int idade;
+	printf("Qual é a sua idade? ");
+	scanf("%d", &idade);
+
+	if(idade < 18){
+		printf("Você é menor de idade.\n");
+	}else if(idade > 18 && idade < 60){
+		printf("Você é adulto.\n");
+	}else{
+		printf("Você é idoso.\n");
+	}
+	printf("Sua idade é %d.", idade);
+
+	return 0;
+}
